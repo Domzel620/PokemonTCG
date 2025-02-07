198fafb9793ea7e472fe4580ece1034c78aee54a
Domzel620 <zelinsk2@go.stockton.edu>
2025-02-07 04:43:20 -0500

    Converted all of the Arrays to Arraylists for more flexibility since the player will constantly be adding and removing from each list. Created getters and started to create setters for the global variables created in PokemonGame. Made a shuffle method and a draw hand method. the draw hand method is essentially the setter for the hand ArrayList. Created a while loop check to ensure that there is at least one Pokemon within the users hand.

--- PokemonTCG/src/TCG/DeckCreator.java
+++ PokemonTCG/src/TCG/DeckCreator.java
@@ -1,45 +1,47 @@
 package src.TCG;
 
+import java.util.ArrayList;
+
 import src.TCG.EnergyCards.EnergyTypes.*;
 import src.TCG.PokemonCards.PokemonCreatures.*;
 
 
 public class DeckCreator {
     //Creates a fire deck filled with 20 charmander and 40 water energy
-    public Card[] createFireDeck(){
-        Card[] fireDeck = new Card[60];
+    public ArrayList<Card> createFireDeck(){
+        ArrayList<Card> fireDeck = new ArrayList<Card>();
         for(int i = 0; i < 20; i++){
-            fireDeck[i] = new Charmander();
+            fireDeck.add(new Charmander());
         }
         for(int i = 20; i < 60; i++){
-            fireDeck[i] = new FireEnergy();
+            fireDeck.add(new FireEnergy());
         }
         return fireDeck;
     }
     //Creates a water deck filled with 20 squirtle and 40 water energy
-    public Card[] createWaterDeck(){
-        Card[] waterDeck = new Card[60];
+    public ArrayList<Card> createWaterDeck(){
+        ArrayList<Card> waterDeck = new ArrayList<Card>();
         for(int i = 0; i < 20; i++){
-            waterDeck[i] = new Squirtle();
+            waterDeck.add(new Squirtle());
         }
         for(int i = 20; i < 60; i++){
-            waterDeck[i] = new WaterEnergy();
+            waterDeck.add(new WaterEnergy());
         }
         return waterDeck;
     }
     //Creates an Electric deck filled with 20 Pikachu and 40 electric energy
-    public Card[] createElectricDeck(){
-        Card[] electricDeck = new Card[60];
+    public ArrayList<Card> createElectricDeck(){
+        ArrayList<Card> electricDeck = new ArrayList<Card>();
         for(int i = 0; i < 20; i++){
-            electricDeck[i] = new Pikachu();
+            electricDeck.add(new Pikachu());
         }
         for(int i = 20; i < 60; i++){
-            electricDeck[i] = new ElectricEnergy();
+            electricDeck.add(new ElectricEnergy());
         }
         return electricDeck;
     }
     //This prints any of the decks that the user creates
-    public void printDeck(Card[] deck){
+    public void printDeck(ArrayList<Card> deck){
         int i = 1;
         for(Card cards : deck){
             System.out.println(i + ". " + cards.getClass().getSimpleName());
--- PokemonTCG/src/TCG/PokemonGame.java
+++ PokemonTCG/src/TCG/PokemonGame.java
@@ -1,14 +1,93 @@
 package src.TCG;
 
-
+import java.util.ArrayList;
+import java.util.Random;
+import src.TCG.PokemonCards.*;
 public class PokemonGame {
-    private Card[] hand;
-    private Card[] deck;
-    private Card[] prize;
-    private Card[] discard;
-    private Card[] bench;
-    private Card[] active;
+    private ArrayList<Card> hand;
+    private ArrayList<Card> deck;
+    private ArrayList<Card> prize;
+    private ArrayList<Card> discard;
+    private ArrayList<Card> bench;
+    private ArrayList<Card> active;
 
+    public ArrayList<Card> getHand(){
+        return hand;
+    }
+    public ArrayList<Card> getDeck(){
+        return deck;
+    }
+    public ArrayList<Card> getPrize(){
+        return prize;
+    }
+    public ArrayList<Card> getDiscard(){
+        return discard;
+    }
+    public ArrayList<Card> getBench(){
+        return bench;
+    }
+    public ArrayList<Card> getActive(){
+        return active;
+    }
+    public void drawHand(ArrayList<Card> userDeck){
+        hand = new ArrayList<Card>();
+        for(int i = 0;  i < 7; i++){
+            hand.add(userDeck.get(i));
+            userDeck.remove(i);
+        }
+        //While loop to check if a Pokemon is present in the hand
+        while (true) { 
+            for(Card cards : hand){
+                if(cards instanceof Pokemon){
+                    System.out.println("Your hand has a pokemon");
+                    break;
+                }
+            }
+            //For loop that readds the card from the hand 
+            //into the deck and then shuffles the deck
+            int k = 0;
+            for(Card cards : hand){
+                userDeck.add(cards);
+                hand.remove(k);
+                k++;  
+            }
+            shuffleDeck(userDeck);
+            //Redraws player hand
+            for(int i = 0;  i < 7; i++){
+                hand.add(userDeck.get(i));
+                userDeck.remove(i);
+            }
+        }   
+    }
+    //This prints the users Hand
+    public void printHand(ArrayList<Card> userHand) {
+        int i = 1;
+        for(Card card : userHand) {
+            System.out.println(i + ". " + card.getClass().getSimpleName());
+            i++; 
+        }
+    }
     
+    public void setDeck(ArrayList<Card> userDeck){
+        deck = userDeck;
+    }
+    //This shuffles the users deck
+    public void shuffleDeck(ArrayList<Card> userDeck){
+        Random shuffle = new Random();
+        for(int i = userDeck.size()-1; i > 0; i--){
+            int j = shuffle.nextInt(i+1);
+            Card temp = userDeck.get(i);
+            userDeck.set(i,  userDeck.get(j));
+            userDeck.set(i, temp);
+        }
+    }
+    //This draws the prize cards
+    public void fillPrize(ArrayList<Card> userDeck){
+        prize = new ArrayList<Card>();
+        for(int i = 0;  i < 7; i++){
+            prize.add(userDeck.get(i));
+            userDeck.remove(i);
+        }
+    }
     
 }
