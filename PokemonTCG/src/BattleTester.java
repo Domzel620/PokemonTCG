7a1d91bdab19176c8e7630aca39ae066a9965e68
Domzel620 <zelinsk2@go.stockton.edu>
2025-03-17 14:27:31 -0400

    Cleaned up Stadium Tester

--- PokemonTCG/src/BattleTester.java
+++ PokemonTCG/src/BattleTester.java
@@ -3,15 +3,7 @@ package src;
 import src.TCG.*;
 public class BattleTester {
     public static void main(String[] args) {
-        //StadiumTester battle = new StadiumTester();
-        //Squirtle coolGuy = new Squirtle();
-        //Charmander hotGuy = new Charmander();
-        //DeckCreator deck1 = new DeckCreator();
         PokemonGame game = new PokemonGame();
-        MonteDeckSimulation monteDeck = new MonteDeckSimulation();
-        //battle.battle(coolGuy, hotGuy);
-       // monteDeck.createMonteDeck();
-       //monteDeck.createMonteRare();
-       game.gameLoop();
+        game.gameLoop();
     }
 }
